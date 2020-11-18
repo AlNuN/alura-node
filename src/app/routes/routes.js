f271896b2d3e4d10013b74e321cb3593cfd64dfe
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-11-18 10:47:08 -0300

    Marko and nodemon setup

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -16,18 +16,8 @@ module.exports = (app) => {
   });
   
   app.get('/livros', (req, resp) => {
-    resp.send(`
-      <!DOCTYPE html>
-      <html lang="pt-br">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-      </head>
-      <body>
-        <h1>Listagem de livros<h1>
-      </body>
-      </html>
-    `);
+    resp.marko(
+      require('../views/livros/lista/lista.marko')
+    );
   });
 }
